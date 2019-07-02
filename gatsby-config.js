6a1dfa19eefbed252e28efb51d74f10eecddb5a9
Josh Mobley <josh@joshmobley.net>
2019-07-02 15:58:29 -0400

    adding favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Josh Mobley`,
+    description: `Web Developer. Wanna-be Athlete.`,
+    author: `@joshmobley`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -26,6 +26,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
+        icon: `static/favicon.png`
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
