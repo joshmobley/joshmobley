1987067f23075f61c04e9e0ec580ec7627ece656
Josh Mobley <josh@joshmobley.net>
2019-07-08 21:13:50 -0400

    adding trailing slash to link for blog entry

--- src/components/BlogEntry.js
+++ src/components/BlogEntry.js
@@ -7,7 +7,7 @@ const BlogEntry = ({ post }) => (
     <div className="blog-entry">
         <div className="blog-entry__content">
             <p className="blog-entry__date">{ post.meta.date } / { post.meta.categories }</p>
-            <Link to={`writing/${post.meta.slug}`}>
+            <Link to={`writing/${post.meta.slug}/`}>
                 <h2 className="blog-entry__title">{ post.meta.title }</h2>
             </Link>
             <p className="blog-entry__text">{ post.meta.excerpt }</p>
